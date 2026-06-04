d7a917471bb29af6e9ee6eb0cde95af089439307
Maciej_Szczesny <mszczesny101@gmail.com>
2026-06-04 15:41:18 +0100

    files updated + actually working

--- bin/main.py
+++ bin/main.py
@@ -7,8 +7,6 @@ from data_loader import load_data
 from train import train_model
 from model import Transformer
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu" # when running locally with newer PyTorch versions with torch.accelerator support
 
 # device = "cuda" if torch.cuda.is_available() else "cpu" # alternative for older PyTorch versions without torch.accelerator, for use on the cluster 
@@ -16,20 +14,7 @@ device = torch.accelerator.current_accelerator().type if torch.accelerator.is_av
 print(f"Using: {device} \n")
 
 X, y, AminoAcids, SecondaryStructures, aa_stoi, aa_itos, aa_encode, aa_decode, ss_stoi, ss_itos, ss_encode, ss_decode, set_length = load_data(set_length=72)
-=======
-=======
->>>>>>> 26dfcf78dc672b2c1db45bf12b1651805314ce44
-# device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu" # when running locally with newer PyTorch versions with torch.accelerator support
 
-device = "cuda" if torch.cuda.is_available() else "cpu" # alternative for older PyTorch versions without torch.accelerator, for use on the cluster 
-
-print(f"Using: {device} \n")
-
-X, y, AminoAcids, SecondaryStructures, aa_stoi, aa_itos, aa_encode, aa_decode, ss_stoi, ss_itos, ss_encode, ss_decode, PAD_LABEL, set_length = load_data(set_length=72)
-<<<<<<< HEAD
->>>>>>> 26dfcf78dc672b2c1db45bf12b1651805314ce44
-=======
->>>>>>> 26dfcf78dc672b2c1db45bf12b1651805314ce44
 dataset = TensorDataset(X, y)
 
 traindata, valdata, testdata = torch.utils.data.random_split(dataset, [0.80, 0.10, 0.10])
@@ -41,8 +26,6 @@ val_loader = DataLoader(valdata, batch_size=32, shuffle=False)
 test_loader = DataLoader(testdata, batch_size=32, shuffle=False)
 
 src_vocab_size = len(AminoAcids) + 1
-<<<<<<< HEAD
-<<<<<<< HEAD
 tgt_vocab_size = len(SecondaryStructures)
 d_model = 64
 num_heads = 2
@@ -50,20 +33,6 @@ num_layers = 3
 d_ff = 4*d_model
 max_seq_length = set_length
 dropout = 0.1
-=======
-=======
->>>>>>> 26dfcf78dc672b2c1db45bf12b1651805314ce44
-tgt_vocab_size = len(SecondaryStructures) + 1
-d_model = 24
-num_heads = 4
-num_layers = 1
-d_ff = 4*d_model
-max_seq_length = set_length
-dropout = 0.2
-<<<<<<< HEAD
->>>>>>> 26dfcf78dc672b2c1db45bf12b1651805314ce44
-=======
->>>>>>> 26dfcf78dc672b2c1db45bf12b1651805314ce44
 
 transformer = Transformer(src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout)
 
@@ -73,18 +42,8 @@ transformer = train_model(
     val_loader=val_loader,
     device=device,
     tgt_vocab_size=tgt_vocab_size,
-<<<<<<< HEAD
-<<<<<<< HEAD
     num_epochs=300,
     learning_rate=1e-4,
-=======
-    num_epochs=100,
-    learning_rate=0.0001,
->>>>>>> 26dfcf78dc672b2c1db45bf12b1651805314ce44
-=======
-    num_epochs=100,
-    learning_rate=0.0001,
->>>>>>> 26dfcf78dc672b2c1db45bf12b1651805314ce44
     save_path="./outputs/weights.pth"
     # save_path=save_path
 )
--- bin/model.py
+++ bin/model.py
@@ -5,10 +5,7 @@ import torch.utils.data as data
 import math
 import copy
 
-<<<<<<< HEAD
 # Encoder-only architecture
-=======
->>>>>>> 26dfcf78dc672b2c1db45bf12b1651805314ce44
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_model, num_heads):
         super(MultiHeadAttention, self).__init__()
@@ -53,17 +50,10 @@ class PositionWiseFeedForward(nn.Module):
         super(PositionWiseFeedForward, self).__init__()
         self.fc1 = nn.Linear(d_model, d_ff)
         self.fc2 = nn.Linear(d_ff, d_model)
-<<<<<<< HEAD
         self.GELU = nn.GELU() # changed from ReLU
 
     def forward(self, x):
         return self.fc2(self.GELU(self.fc1(x)))
-=======
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        return self.fc2(self.relu(self.fc1(x)))
->>>>>>> 26dfcf78dc672b2c1db45bf12b1651805314ce44
     
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_seq_length):
@@ -81,7 +71,6 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         return x + self.pe[:, :x.size(1)]
 
-<<<<<<< HEAD
 class LocalConv(nn.Module):
     def __init__(self, d_model, kernel_size=7, dropout=0.1):
         super().__init__()
@@ -146,77 +135,3 @@ class Transformer(nn.Module):
 
         output = self.fc(x)
         return output
-=======
-class EncoderLayer(nn.Module):
-    def __init__(self, d_model, num_heads, d_ff, dropout):
-        super(EncoderLayer, self).__init__()
-        self.self_attn = MultiHeadAttention(d_model, num_heads)
-        self.feed_forward = PositionWiseFeedForward(d_model, d_ff)
-        self.norm1 = nn.LayerNorm(d_model)
-        self.norm2 = nn.LayerNorm(d_model)
-        self.dropout = nn.Dropout(dropout)
-        
-    def forward(self, x, mask):
-        attn_output = self.self_attn(x, x, x, mask)
-        x = self.norm1(x + self.dropout(attn_output))
-        ff_output = self.feed_forward(x)
-        x = self.norm2(x + self.dropout(ff_output))
-        return x
-    
-class DecoderLayer(nn.Module):
-    def __init__(self, d_model, num_heads, d_ff, dropout):
-        super(DecoderLayer, self).__init__()
-        self.self_attn = MultiHeadAttention(d_model, num_heads)
-        self.cross_attn = MultiHeadAttention(d_model, num_heads)
-        self.feed_forward = PositionWiseFeedForward(d_model, d_ff)
-        self.norm1 = nn.LayerNorm(d_model)
-        self.norm2 = nn.LayerNorm(d_model)
-        self.norm3 = nn.LayerNorm(d_model)
-        self.dropout = nn.Dropout(dropout)
-        
-    def forward(self, x, enc_output, src_mask, tgt_mask):
-        attn_output = self.self_attn(x, x, x, tgt_mask)
-        x = self.norm1(x + self.dropout(attn_output))
-        attn_output = self.cross_attn(x, enc_output, enc_output, src_mask)
-        x = self.norm2(x + self.dropout(attn_output))
-        ff_output = self.feed_forward(x)
-        x = self.norm3(x + self.dropout(ff_output))
-        return x
-    
-class Transformer(nn.Module):
-    def __init__(self, src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout):
-        super(Transformer, self).__init__()
-        self.encoder_embedding = nn.Embedding(src_vocab_size, d_model)
-        self.decoder_embedding = nn.Embedding(tgt_vocab_size, d_model)
-        self.positional_encoding = PositionalEncoding(d_model, max_seq_length)
-
-        self.encoder_layers = nn.ModuleList([EncoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
-        self.decoder_layers = nn.ModuleList([DecoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
-
-        self.fc = nn.Linear(d_model, tgt_vocab_size)
-        self.dropout = nn.Dropout(dropout)
-
-    def generate_mask(self, src, tgt):
-        src_mask = (src != 0).unsqueeze(1).unsqueeze(2)
-        tgt_mask = (tgt != 0).unsqueeze(1).unsqueeze(3)
-        seq_length = tgt.size(1)
-        nopeak_mask = torch.tril(torch.ones(1, seq_length, seq_length, device=tgt.device, dtype=torch.bool))
-        tgt_mask = tgt_mask & nopeak_mask
-        return src_mask, tgt_mask
-
-    def forward(self, src, tgt):
-        src_mask, tgt_mask = self.generate_mask(src, tgt)
-        src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src)))
-        tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(tgt)))
-
-        enc_output = src_embedded
-        for enc_layer in self.encoder_layers:
-            enc_output = enc_layer(enc_output, src_mask)
-
-        dec_output = tgt_embedded
-        for dec_layer in self.decoder_layers:
-            dec_output = dec_layer(dec_output, enc_output, src_mask, tgt_mask)
-
-        output = self.fc(dec_output)
-        return output
->>>>>>> 26dfcf78dc672b2c1db45bf12b1651805314ce44
